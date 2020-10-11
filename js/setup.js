5f3bb98847b9f2e0ac9fdb6eb77680118a10d053
EnjoyTWOS <danylyuk.v@gmail.com>
2020-10-11 23:35:41 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,21 +4,29 @@ const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `
 const WIZARD_SURAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const wizardsQuantity = 4;
 const wizards = [];
-
-const userDialog = document.querySelector(`.setup`);
-userDialog.classList.remove(`hidden`);
-
-const similarListElement = userDialog.querySelector(`.setup-similar-list`);
+const fragment = document.createDocumentFragment();
+const userDialogTemplate = document.querySelector(`.setup`);
+const similarListElement = userDialogTemplate.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 
+
+const showUserDialogTemplate = () => {
+  userDialogTemplate.classList.remove(`hidden`);
+};
+
+const showWizardsTemplate = () => {
+  userDialogTemplate.querySelector(`.setup-similar`).classList.remove(`hidden`);
+};
+
 const getRandomArrI = (arr) => {
-  let rand = Math.floor(Math.random() * arr.length);
+  const rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
 };
 
 const renderWizardsArr = () => {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < wizardsQuantity; i++) {
     const wizardTemplate = {
       name: getRandomArrI(WIZARD_NAMES) + ` ` + getRandomArrI(WIZARD_SURAMES),
       coatColor: getRandomArrI(COAT_COLORS),
@@ -29,7 +37,13 @@ const renderWizardsArr = () => {
   return wizards;
 };
 
-renderWizardsArr();
+const init = () => {
+  showWizardsTemplate();
+  showUserDialogTemplate();
+  renderWizardsArr();
+};
+
+init();
 
 const renderWizard = (wizard) => {
   let wizardElement = similarWizardTemplate.cloneNode(true);
@@ -41,11 +55,8 @@ const renderWizard = (wizard) => {
   return wizardElement;
 };
 
-const fragment = document.createDocumentFragment();
-
 for (let i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
 similarListElement.appendChild(fragment);
-userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
