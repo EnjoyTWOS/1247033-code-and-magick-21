4fedeb30457b9b99ceaa56d8555ef7902c045884
EnjoyTWOS <danylyuk.v@gmail.com>
2020-11-02 18:52:04 +0200

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@ const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `
 const WIZARD_SURAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
-const wizardsQuantity = 4;
+const WIZARDS_QUANTITY = 4;
 const wizards = [];
 const fragment = document.createDocumentFragment();
 const userDialogTemplate = document.querySelector(`.setup`);
@@ -26,7 +26,7 @@ const getRandomArrI = (arr) => {
 };
 
 const renderWizardsArr = () => {
-  for (let i = 0; i < wizardsQuantity; i++) {
+  for (let i = 0; i < WIZARDS_QUANTITY; i++) {
     const wizardTemplate = {
       name: getRandomArrI(WIZARD_NAMES) + ` ` + getRandomArrI(WIZARD_SURAMES),
       coatColor: getRandomArrI(COAT_COLORS),
@@ -41,9 +41,9 @@ const init = () => {
   showWizardsTemplate();
   showUserDialogTemplate();
   renderWizardsArr();
+  renderWizardsTemplate();
 };
 
-init();
 
 const renderWizard = (wizard) => {
   let wizardElement = similarWizardTemplate.cloneNode(true);
@@ -55,8 +55,11 @@ const renderWizard = (wizard) => {
   return wizardElement;
 };
 
-for (let i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+const renderWizardsTemplate = () => {
+  for (let i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
 
-similarListElement.appendChild(fragment);
+init();
