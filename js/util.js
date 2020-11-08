6f7c355ceb5615669c10dfe2925a0dc9ec4f6b83
EnjoyTWOS <danylyuk.v@gmail.com>
2020-11-09 01:34:43 +0200

    m5-t1

---
+++ js/util.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function () {
+  const ENTER_KEY = `Enter`;
+  const ESCAPE_KEY = `Escape`;
+
+  window.util = {
+    isEscEvent(evt, action) {
+      if (evt.keyCode === ESCAPE_KEY) {
+        evt.preventDefault();
+        action();
+      }
+    },
+    isEnterEvent(evt, action) {
+      if (evt.keyCode === ENTER_KEY) {
+        action();
+      }
+    },
+    getRandomArrI(arr) {
+      const rand = Math.floor(Math.random() * arr.length);
+      return arr[rand];
+    },
+    colorize(element, elementColor, elementInput) {
+      element.addEventListener(`click`, function () {
+        const color = window.util.getRandomArrI(elementColor);
+        if (element.tagName.toLowerCase() === `div`) {
+          element.style.backgroundColor = color;
+          elementInput.value = color;
+        } else {
+          element.style.fill = color;
+          elementInput.value = element.style.fill;
+        }
+      });
+    }
+  };
+}
+)();
